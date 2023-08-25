3e401e2581624c3e5695a3ba477edfc89e8c953e
Om Jaju <113230517+omjaju18@users.noreply.github.com>
2023-08-25 22:40:18 +0530

    Feat: Updated Modals

--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -1,22 +1,27 @@
-"use client";
 
-import { useEffect, useState } from "react";
+"use client"; 
+import { useEffect, useState } from "react"; 
+import { StoreModal } from "@/components/modals/store-modal"; 
 
-import { StoreModal } from "@/components/modals/store-modal";
-
+// Define a React component named ModalProvider
 export const ModalProvider = () => {
-    const [isMounted, setIsMOunted] = useState(false);
+    // Define a state variable to track whether the component is mounted
+    const [isMounted, setIsMounted] = useState(false);
 
+    // Use the useEffect hook to set isMounted to true when the component mounts
     useEffect(() => {
-        setIsMOunted(true);
-    }, [])
+        setIsMounted(true);
+    }, []);
 
+    // If the component is not yet mounted, return null (component is not rendered)
     if (!isMounted) {
-        return null
+        return null;
     }
+
+    // If the component is mounted, render the StoreModal component
     return (
         <>
             <StoreModal />
         </>
-    )
+    );
 }
--- providers/toast-provider.tsx
+++ providers/toast-provider.tsx
@@ -2,6 +2,7 @@
 
 import { Toaster } from "react-hot-toast";
 
+//Toast used for fancy alerts
 export const ToastProvider = () => {
     return (
         <Toaster />
