c9ae83fea4ff1d9c72feb6fd3a4d9cd9104c7e17
Om Jaju <113230517+omjaju18@users.noreply.github.com>
2023-08-26 11:58:30 +0530

    Use-origin Hook

---
+++ hooks/use-origin.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+// Define a custom React hook named 'useOrigin'
+export const useOrigin = () => {
+    // State to track whether the component is mounted
+    const [mounted, setMounted] = useState(false);
+
+    // Calculate the 'origin' based on the client-side environment
+    const origin = typeof window !== 'undefined' && window.location.origin ? window.location.origin : '';
+
+    // Effect to set 'mounted' to true when the component is mounted
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    // Check if the component is not yet mounted, return an empty string
+    if (!mounted) {
+        return '';
+    }
+
+    // Return the calculated 'origin'
+    return origin;
+};
