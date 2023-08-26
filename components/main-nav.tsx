25095bf5ba2c0644937ce8fcd015e58b465f2ef8
Om Jaju <113230517+omjaju18@users.noreply.github.com>
2023-08-26 11:40:49 +0530

    Created Overview Button

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -17,6 +17,11 @@ export function MainNav({
     // Define an array of routes with href, label, and active properties.
     const routes = [
         {
+            href: `/${params.storeId}`,
+            label: 'Overview',
+            active: pathname === `/${params.storeId}`,
+        },
+        {
             href: `/${params.storeId}/settings`,
             label: 'Settings',
             active: pathname === `/${params.storeId}/settings`,
