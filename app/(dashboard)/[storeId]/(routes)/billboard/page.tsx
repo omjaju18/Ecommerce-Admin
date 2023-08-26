58f69f4661aa9c0c49c9863cfa98f22de57ea53b
Om Jaju <113230517+omjaju18@users.noreply.github.com>
2023-08-26 12:36:19 +0530

    Added billboard link

---
+++ app/(dashboard)/[storeId]/(routes)/billboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BillboardPage = () => {
+    return (
+        <div>
+            Billboards
+            
+        </div>
+    )
+}
+
+export default BillboardPage
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -22,6 +22,11 @@ export function MainNav({
             active: pathname === `/${params.storeId}`,
         },
         {
+            href: `/${params.storeId}/billboard`,
+            label: 'Billboard',
+            active: pathname === `/${params.storeId}/billboard`,
+        },
+        {
             href: `/${params.storeId}/settings`,
             label: 'Settings',
             active: pathname === `/${params.storeId}/settings`,
