f67704c9597722a4c2f5f4efd4b47e5c3794bb9f
Om Jaju <113230517+omjaju18@users.noreply.github.com>
2023-08-27 15:42:57 +0530

    Columns Added

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/columns.tsx
@@ -1,26 +1,32 @@
-"use client"
+// Import necessary types and components
+import { ColumnDef } from "@tanstack/react-table";
+import { CellAction } from "./cell-action";
 
-import { ColumnDef } from "@tanstack/react-table"
-
-import { CellAction } from "./cell-action"
-
+// Define the type representing the structure of a billboard
 export type BillboardColumn = {
-    id: string
+    id: string;
     label: string;
     createdAt: string;
 }
 
+// Define an array of column definitions for the table
 export const columns: ColumnDef<BillboardColumn>[] = [
     {
+        // Define the data source for this column
         accessorKey: "label",
+        // Define the header label for this column
         header: "Label",
     },
     {
+        // Define the data source for this column
         accessorKey: "createdAt",
+        // Define the header label for this column
         header: "Date",
     },
     {
+        // Define a special column for displaying actions
         id: "actions",
+        // Define how the cell content should be rendered for this column
         cell: ({ row }) => <CellAction data={row.original} />
     },
 ];
